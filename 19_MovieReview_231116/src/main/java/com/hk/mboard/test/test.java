d04a0668d327d4d3f2de7549922f811bebd2160e
HKEDU <HKEDU@DESKTOP-UMP4GPR>
2023-11-21 14:32:51 +0900

    제발

--- 19_MovieReview_231116/src/main/java/com/hk/mboard/test/test.java
+++ 19_MovieReview_231116/src/main/java/com/hk/mboard/test/test.java
@@ -3,7 +3,7 @@ package com.hk.mboard.test;
 public class test {
 	private void test01() {
 		// TODO Auto-generated method stub
-		System.out.println("보여라 얍");
+		System.out.println("보여라 얍dd");
 		
 	}
 }
