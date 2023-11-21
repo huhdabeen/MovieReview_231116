bb13e98e07fd0b5cb68d91f9f27244ec4d0896c2
HKEDU <HKEDU@DESKTOP-D15LC3P>
2023-11-21 14:34:58 +0900

    다빈바보

--- 19_MovieReview_231116/src/main/java/com/hk/mboard/test/test.java
+++ 19_MovieReview_231116/src/main/java/com/hk/mboard/test/test.java
@@ -3,7 +3,7 @@ package com.hk.mboard.test;
 public class test {
 	private void test01() {
 		// TODO Auto-generated method stub
-		System.out.println("보여라 얍dd");
+		System.out.println("어우 너무 졸려요");
 		
 	}
 }
